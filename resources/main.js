2d24fb60524b72a7186b1c8d624ba61af6086708
AK Krajewska <akrajewska@gmail.com>
2017-11-26 13:49:49 -0800

    session timer and break timer work

--- resources/main.js
+++ resources/main.js
@@ -1,6 +1,4 @@
 //borrowed code
-
-
     var seconds = 60;
     function secondPassed() {
         var minutes = Math.round((seconds - 30)/60);
@@ -21,11 +19,57 @@
 //end borrowed code
 
 
+//this is the default duration, but it can be over written with interation
+var sessionMinutes = 1;
+var breakMinutes = 1;
+
+function countDownTimer(duration){
+    //runs the timer
+    var onBreak = false;
+    var durationSeconds = (duration * 60);
+    console.log("I'm starting a timer with a duration in seconds of " + durationSeconds);
+   
+    function tickTock(){
+        //if the duration is 1 second or more than a second sutract 1 from the display
+        //if the duration is 0, stop the function????
+        
+        
+        if (durationSeconds >= 1){
+            durationSeconds = durationSeconds - 1;
+            var remainingMinutes = Math.round((durationSeconds - 30)/60); 
+            var remainingSeconds = durationSeconds % 60;
+            if (onBreak == false) {
+                document.getElementById("session").innerHTML = "SESSION: remainingMinutes: " + remainingMinutes + " remainingSeconds: " + remainingSeconds;
+                document.getElementById("break").innerHTML = "BREAK: " + breakMinutes;
+            } else if (onBreak == true){
+                document.getElementById("break").innerHTML = "BREAK: remainingMinutes: " + remainingMinutes + " remainingSeconds: " + remainingSeconds;
+            }
+            
+        }
+        if (durationSeconds == 0){
+            if (onBreak == false){
+                document.getElementById("session").innerHTML = "Session complete";
+                onBreak = true;
+                durationSeconds = breakMinutes *60;
+                tickTock();
+            //call the break functions
+            } else if ((onBreak == true) && (durationSeconds == 0)){
+                document.getElementById("break").innerHTML = "Break complete";
+            }
+            
+        }
+        
+    }
+    var secondInterval = setInterval(tickTock, 1000);
+  }
+
+
+
 
 function timerControl(command){    
     if (command == "start"){
         console.log("start the clock");
-        countDownTimer(3);
+        countDownTimer(sessionMinutes);
         //run the time function
     }
     else if (command == "pause"){
@@ -46,31 +90,6 @@ function timerControl(command){
     }
 }
 
-//this is the default duration, but it can be over written with interation
-var sessionMinutes = 25;
-var breakMinutes = 5;
-
-function countDownTimer(duration){
-    //runs the timer
-    
-    var durationSeconds = (duration * 60);
-    console.log("I'm starting a timer with a duration in seconds of " + durationSeconds);
-    document.getElementById("session").innerHTML = durationSeconds;
-    function tickTock(){
-        //if the duration is 1 second or more than a second sutract 1 from the display
-        //if the duration is 0, stop the function????
-        if (durationSeconds >= 1){
-            durationSeconds = durationSeconds - 1;
-            document.getElementById("session").innerHTML = durationSeconds;
-        }
-        
-    }
-    var secondInterval = setInterval(tickTock, 1000);
-    
-    
-    
-    
-}
 
 function setTime(time){
     //customizes the length of the timer
