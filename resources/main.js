b37f43cc3e74eb008185be4db5b64b1276e4a5cf
AK Krajewska <akrajewska@gmail.com>
2017-11-26 16:23:10 -0800

    buttons to increment and decrement session and break time

--- resources/main.js
+++ resources/main.js
@@ -1,72 +1,80 @@
-//borrowed code
-    var seconds = 60;
-    function secondPassed() {
-        var minutes = Math.round((seconds - 30)/60);
-        var remainingSeconds = seconds % 60;
-        if (remainingSeconds < 10) {
-            remainingSeconds = "0" + remainingSeconds;  
-        }
-        document.getElementById('sessionTimer').innerHTML = minutes + ":" + remainingSeconds;
-        if (seconds == 0) {
-            clearInterval(countdownTimer);
-            document.getElementById('sessionTimer').innerHTML = "Buzz Buzz";
-        } else {
-            seconds--;
-        }
-    }
-    var countdownTimer = setInterval('secondPassed()', 1000);
-
-//end borrowed code
-
-
 //this is the default duration, but it can be over written with interation
 var sessionMinutes = 1;
 var breakMinutes = 1;
-
-//function countDownTimer(){
-    //runs the timer
-    var onBreak = false;
-    var durationSeconds = (breakMinutes * 60);
-    console.log("I'm starting a timer with a duration in seconds of " + durationSeconds);
-   
-    function tickTock(){
-        //if the duration is 1 second or more than a second sutract 1 from the display
-        //if the duration is 0, stop the function????
-        
-        if (durationSeconds >= 1){
-            durationSeconds = durationSeconds - 1;
-            var remainingMinutes = Math.round((durationSeconds - 30)/60); 
-            var remainingSeconds = durationSeconds % 60;
-            if (onBreak == false) {
-                document.getElementById("session").innerHTML = "SESSION: remainingMinutes: " + remainingMinutes + " remainingSeconds: " + remainingSeconds;
-                document.getElementById("break").innerHTML = "BREAK: " + breakMinutes;
-            } else if (onBreak == true){
-                document.getElementById("break").innerHTML = "BREAK: remainingMinutes: " + remainingMinutes + " remainingSeconds: " + remainingSeconds;
-            }
-            
-        }
-        if (durationSeconds == 0){
-            if (onBreak == false){
-                document.getElementById("session").innerHTML = "Session complete";
-                onBreak = true;
-                durationSeconds = breakMinutes *60;
-                tickTock();
-            //call the break functions
-            } else if ((onBreak == true) && (durationSeconds == 0)){
-                document.getElementById("break").innerHTML = "Break complete";
-            }
-            
-        }
-        
-    }
-    //var secondInterval = setInterval(tickTock, 1000);
-  //}
-
-
+var onBreak = false;
+var durationSeconds = (sessionMinutes * 60);
 var secondInterval;
 
-//rewrite each timercontrol into a seperate funciton
+//show the timer with the selected break and session time
+function showTimer(){
+    document.getElementById("session").innerHTML = "SESSION: " + sessionMinutes + ":00";
+    document.getElementById("break").innerHTML = "BREAK: " + breakMinutes + ":00";
+}
 
+
+//this might want to be several timers
+//write safeteys in so you can't decrement lower thatn 1, otherwise you get negative time
+//write safeteys so you can't mess with a session in progress
+
+function incrementSession(){
+    sessionMinutes = sessionMinutes + 1;
+    console.log("sessionMinutes is now: " + sessionMinutes);
+    durationSeconds = sessionMinutes * 60;
+    showTimer();
+    //button to increment session by 1 minute
+}
+
+function decrementSession(){
+    //button to decrement session by 1 minute
+    sessionMinutes = sessionMinutes - 1;
+    console.log("sessionMinutes is now: " + sessionMinutes);
+    durationSeconds = sessionMinutes * 60;
+    showTimer();
+}
+
+function incrementBreak(){
+    //button to increment berak by 1 minute
+    breakMinutes = breakMinutes + 1;
+    console.log("breakMinutes is now: " + breakMinutes);
+    //durationSeconds = sessionMinutes * 60;
+    showTimer();
+}
+
+function decrementBreak(){
+    //button to decrement break by 1 minute
+    breakMinutes = breakMinutes - 1;
+    console.log("breakMinutes is now: " + breakMinutes);
+}
+
+//runs the timer
+function tickTock(){
+        //if the duration is 1 second or more than a second sutract 1 from the display
+        //if the duration is 0, stop the function????
+      
+    if (durationSeconds >= 1){
+        durationSeconds = durationSeconds - 1;
+        var remainingMinutes = Math.round((durationSeconds - 30)/60); 
+        var remainingSeconds = durationSeconds % 60;
+        if (onBreak == false) {
+            document.getElementById("session").innerHTML = "SESSION: " + remainingMinutes + ":" + remainingSeconds;
+            document.getElementById("break").innerHTML = "BREAK: " + breakMinutes + ":00";
+        } else if (onBreak == true){
+            document.getElementById("break").innerHTML = "BREAK: " + remainingMinutes + ":" + remainingSeconds;
+        }
+            
+    } else {
+        if (onBreak == false){
+            document.getElementById("session").innerHTML = "Session complete";
+            onBreak = true;
+            durationSeconds = breakMinutes * 60;
+            tickTock();
+            //call the break functions
+        } else if ((onBreak == true) && (durationSeconds == 0)){
+            document.getElementById("break").innerHTML = "Break complete";
+        }
+    }
+}
+   
 function startTimer(){
     console.log("start the clock");
     secondInterval = setInterval(tickTock, 1000);
@@ -84,18 +92,6 @@ function resetTimer(){
     if (confirm("Are you sure you want to stop this potato early?")){
         console.log("reset the clock"); 
         clearInterval(secondInterval); 
-        document.getElementById("session").innerHTML = "SESSION: " + sessionMinutes;
-        document.getElementById("break").innerHTML = "BREAK: " + breakMinutes;
-            
-        //set the timer to starting postion but don't start it
-        } else {
-            //do nothing
-        }
-}
-
-
-
-function setTime(time){
-    //customizes the length of the timer
-    //change the variable to something other than time because time is often the name of a built in library or funcion
+        showTimer();
+        } 
 }
